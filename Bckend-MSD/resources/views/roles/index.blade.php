08fd3201fe6112576c2da3b9f5cbf8ea13080073
Ivett <utmivett@gmail.com>
2025-07-04 00:39:11 -0600

    acomodo de carpetas - s

--- Bckend-MSD/resources/views/roles/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Lista de Roles</h1>
-<ul>
-    @foreach ($roles as $role)
-        <li>
-            {{ $role->name }}
-            <a href="{{ route('roles.edit', $role->id) }}">Editar permisos</a>
-        </li>
-    @endforeach
-</ul>
